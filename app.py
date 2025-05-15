cf7039a82330a63210d5a0409b5549f6d4371063
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-15 17:02:48 -0300

    feat: add lista_usuarios feature

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 lista_usuarios = []
 
 def cadastrar_usuario():
+    print("\n--- Cadastro de Usuário ---")
+
     nome = input('Nome: ')
     email = input('Email: ')
     idade = int(input('Idade: '))
@@ -21,3 +23,16 @@ def cadastrar_usuario():
         'email': email,
         'idade': idade
     }
+
+    lista_usuarios.append(usuario)
+    return usuario
+
+def listar_usuarios():
+    print("\n--- Lista de Usuários ---")
+    
+    if not lista_usuarios:
+        print('Nenhum usuário cadastrado.')
+        return
+
+    for usuario in lista_usuarios:
+        print(f"Nome: {usuario['nome']}, Email: {usuario['email']}, Idade: {usuario['idade']}")
